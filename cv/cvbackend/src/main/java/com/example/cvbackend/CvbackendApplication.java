9753ec943675df6c0868d491919eff0702ec2e0f
alee0050 <alee0050@student.monash.edu>
2023-12-11 00:30:17 +1100

    setup env

--- cv/cvbackend/src/main/java/com/example/cvbackend/CvbackendApplication.java
+++ cv/cvbackend/src/main/java/com/example/cvbackend/CvbackendApplication.java
@@ -2,12 +2,30 @@ package com.example.cvbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.cvbackend.api.Greeting;
 
 @SpringBootApplication
 public class CvbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CvbackendApplication.class, args);
+    System.out.println("Hello, This is coming from the backend!");
 	}
 
+@RestController
+@RequestMapping("/api")
+class GreetingController {
+
+    @GetMapping("/greet")
+    public Greeting greet() {
+        return new Greeting("Hello, this is your API greeting!");
+    }
+}
+
+
+
+  Function 
 }
---
+++ cv/cvbackend/src/main/java/com/example/cvbackend/api/Greeting.java
@@ -0,0 +1,16 @@
+// src/main/java/com/example/cvbackend/api/Greeting.java
+//
+package com.example.cvbackend.api;
+
+public class Greeting {
+    private final String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
