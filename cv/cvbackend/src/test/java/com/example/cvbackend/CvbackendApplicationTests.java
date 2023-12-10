3510fa3116f268ddbab71753f4c7a9694023199f
alee0050 <alee0050@student.monash.edu>
2023-12-10 21:05:25 +1100

    Setup for maven and cvbackend

---
+++ cv/cvbackend/src/test/java/com/example/cvbackend/CvbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cvbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
